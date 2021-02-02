d88a0a45869e4be6961379722a167e1b47482210
Aleck Bowen Sun <aleck.bowen.sun@gmail.com>
2021-02-01 20:00:02 -0500

    changed route

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -9,7 +9,7 @@ function HomePage() {
             <h1 className = 'home-title'>Welcome to <span className='home-logo'>Projectify</span></h1>
             <p className = 'home-desc'>Get inspiration from innovative projects and show off your<br></br>
             own amazing creations!</p>
-            <Link to='/projectsPage' className='start-button'>
+            <Link to='/postPage' className='start-button'>
                 Get Started <i className="fas fa-arrow-right"></i>
             </Link>
         </div>
